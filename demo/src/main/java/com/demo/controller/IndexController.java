2dca1a9d70f7288909f945b08efb4e38b6b786aa
lirong <18292800@qq.com>
2019-08-23 16:38:35 +0800

    迁移

--- demo/src/main/java/com/demo/controller/IndexController.java
+++ demo/src/main/java/com/demo/controller/IndexController.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * class TempController
+ * class IndexController
  *
  * @author lirong
  */
 @RestController
-public class TempController {
-    @GetMapping("/test")
-    public String test() {
+public class IndexController {
+    @GetMapping("/index")
+    public String index() {
         return "欢迎登录";
     }
 }
