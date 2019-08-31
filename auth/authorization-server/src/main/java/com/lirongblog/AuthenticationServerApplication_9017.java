0b8070f80763ad5659e149f7c2e147b0573c158b
lirong <18292800@qq.com>
2019-08-31 16:05:09 +0800

    重构

---
+++ auth/authorization-server/src/main/java/com/lirongblog/AuthenticationServerApplication_9017.java
@@ -0,0 +1,12 @@
+package com.lirongblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServerApplication_9017 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServerApplication_9017.class, args);
+    }
+}
