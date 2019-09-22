c96a2bd9b555b613d5f0476a968cd4a2bb33d655
lirong <18292800@qq.com.com>
2019-09-22 17:17:07 +0800

    更新代码

---
+++ auth/authorization-service/src/main/java/com/gxlirong/auth/authentication/Oauth2AuthorizationApplication_8091.java
@@ -0,0 +1,20 @@
+package com.gxlirong.auth.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+/**
+ * class Oauth2AuthorizationApplication_8091
+ *
+ * @author lirong
+ */
+@EnableHystrix //开启 Hystrix 熔断机制的支持
+@EnableEurekaClient
+@SpringBootApplication
+public class Oauth2AuthorizationApplication_8091 {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2AuthorizationApplication_8091.class, args);
+    }
+}
